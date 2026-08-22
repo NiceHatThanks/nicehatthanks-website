1b57f1cbeb9036cfea4015915defdff7dc0a530e
NiceHatThanks <63918742+NiceHatThanks@users.noreply.github.com>
2026-08-22 14:52:00 +0100

    Bust checkout cart script cache

--- js/shop-options.js
+++ js/shop-options.js
@@ -93,5 +93,5 @@ checkoutStatusScript.src = 'js/checkout-status.js?v=20260822-2';
 document.body.appendChild(checkoutStatusScript);
 
 const checkoutTestScript = document.createElement('script');
-checkoutTestScript.src = 'js/checkout-test.js?v=20260822-2';
+checkoutTestScript.src = 'js/checkout-test.js?v=20260822-3';
 document.body.appendChild(checkoutTestScript);
