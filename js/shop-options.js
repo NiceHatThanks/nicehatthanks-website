36a3ba0b69e7db2d589ef44149009a62e0861cfa
NiceHatThanks <63918742+NiceHatThanks@users.noreply.github.com>
2026-08-22 15:28:52 +0100

    Bump checkout polish cache version

--- js/shop-options.js
+++ js/shop-options.js
@@ -97,5 +97,5 @@ checkoutTestScript.src = 'js/checkout-test.js?v=20260822-3';
 document.body.appendChild(checkoutTestScript);
 
 const checkoutPolishScript = document.createElement('script');
-checkoutPolishScript.src = 'js/checkout-polish.js?v=20260822-5';
+checkoutPolishScript.src = 'js/checkout-polish.js?v=20260822-6';
 document.body.appendChild(checkoutPolishScript);
